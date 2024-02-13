daed25e55dec76aa6b49182dcd78422564fef460
DJAE <djaeardi80@gmail.com>
2024-02-13 16:57:48 +0100

    Alert email existant, et control bouton disabled

--- navigation/screens/LoginScreen.js
+++ navigation/screens/LoginScreen.js
@@ -7,6 +7,20 @@ import firebase from '../../config/firebase';
 export default function LoginScreen({ navigation }) {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [isButtonDisabled, setIsButtonDisabled] = useState(true); // état pour activer/désactiver le bouton Se connecter
+
+  // Fonction pour vérifier si les champs email et mot de passe sont remplis
+  const checkInputs = () => {
+    if (email.trim() !== '' && password.trim() !== '') {
+      setIsButtonDisabled(false); // activer le bouton si les champs sont remplis
+    } else {
+      setIsButtonDisabled(true); // désactiver le bouton si un champ est vide
+    }
+  };
+
+  useEffect(() => {
+    checkInputs(); // Vérifier à chaque changement des champs email et mot de passe
+  }, [email, password])
 
   useEffect(() => {
     const unsubscribe = firebase.auth().onAuthStateChanged(user => {
@@ -19,13 +33,30 @@ export default function LoginScreen({ navigation }) {
   }, );
 
   const handleSignUp = () => {
-    firebase.auth()
-      .createUserWithEmailAndPassword(email, password)
-      .then(userCredentials => {
-        const user = userCredentials.user;
-        console.log('Registered with:', user.email);
-      })
-      .catch(error => alert(error.message));
+    // Vérifier d'abord si l'utilisateur existe déjà avec cette adresse e-mail
+    firebase
+    .auth()
+    .fetchSignInMethodsForEmail(email)
+    .then(signInMethods => {
+      if (signInMethods && signInMethods.length > 0) {
+        // L'utilisateur existe déjà, afficher une alerte
+        Alert.alert(
+          'Adresse e-mail déjà utilisée',
+          'Un compte avec cette adresse e-mail existe déjà. Veuillez utiliser une autre adresse e-mail.'
+        );
+      } else {
+        // L'utilisateur n'existe pas encore, créer un nouveau compte
+        firebase
+          .auth()
+          .createUserWithEmailAndPassword(email, password)
+          .then(userCredentials => {
+            const user = userCredentials.user;
+            console.log('Registered with:', user.email);
+          })
+          .catch(error => alert(error.message));
+      }
+    })
+    .catch(error => alert(error.message));
   };
 
   const handleLogin = () => {
@@ -58,7 +89,10 @@ export default function LoginScreen({ navigation }) {
           style={styles.input}
           placeholder="Email"
           value={email}
-          onChangeText={setEmail}
+          onChangeText={text => {
+            setEmail(text);
+            checkInputs(); // Vérifier à chaque changement de texte
+          }}
           keyboardType="email-address"
           autoCapitalize="none"
           autoCompleteType="email"
@@ -69,20 +103,39 @@ export default function LoginScreen({ navigation }) {
           style={styles.input}
           placeholder="Mot de passe"
           value={password}
-          onChangeText={setPassword}
+          onChangeText={text => {
+            setPassword(text);
+            checkInputs(); // Vérifier à chaque changement de texte
+          }}
           secureTextEntry
           autoCapitalize="none"
           autoCompleteType="password"
           textContentType="password"
           placeholderTextColor="#000"
         />
-        <TouchableOpacity style={styles.button} onPress={handleLogin} >
+        <TouchableOpacity
+          style={[styles.button, styles.connectButton, isButtonDisabled && styles.disabledButton]}
+          onPress={handleLogin}
+          disabled={isButtonDisabled}
+        >
           <Text style={styles.buttonText}>Se connecter</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={styles.button} onPress={handleSignUp}>
+        <TouchableOpacity
+          style={[styles.button, styles.signUpButton, isButtonDisabled && styles.disabledButton]}
+          onPress={handleSignUp}
+          disabled={isButtonDisabled}
+        >
           <Text style={styles.buttonText}>S'inscrire</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={styles.button} onPress={handleResetPassword}>
+        <TouchableOpacity
+          style={[
+            styles.button,
+            styles.forgotPasswordButton,
+            email.trim() === '' && styles.disabledButton // Désactiver le bouton si le champ email est vide
+          ]}
+          onPress={handleResetPassword}
+          disabled={email.trim() === ''}
+        >
           <Text style={styles.buttonText}>Mot de passe oublié ?</Text>
         </TouchableOpacity>
       </View>
@@ -124,7 +177,19 @@ const styles = StyleSheet.create({
   },
   buttonText: {
     color: '#FFFFFF',
-    fontWeight: 'bold',
     fontSize: 16,
+    fontWeight: 'bold',
+  },
+  connectButton: {
+    backgroundColor: '#007AFF', // couleur pour le bouton Se connecter
+  },
+  signUpButton: {
+    backgroundColor: '#FFA500', // couleur pour le bouton S'inscrire
+  },
+  forgotPasswordButton: {
+    backgroundColor: '#FF6347', // couleur pour le bouton Mot de passe oublié
+  },
+  disabledButton: {
+    backgroundColor: 'rgba(169, 169, 169, 0.5)', // couleur pour le bouton désactivé
   },
 });
